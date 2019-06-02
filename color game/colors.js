82d9ac0b6150090837af0ff38688daaa63801d8b
Kristian Arbutina <arbutinachristian@gmail.com>
2019-06-02 22:31:19 +0200

    Add files via upload

---
+++ color game/colors.js
@@ -0,0 +1,80 @@
+var numberOfSquares = 6
+var colors = generateRandomColors(numberOfSquares );
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var choices = document.getElementById("choice");
+var message = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var modeButtons = document.getElementsByClassName("mode");
+choices.textContent = pickedColor;
+
+for (var i = 0; i < modeButtons.length; i++) {
+    modeButtons[i].addEventListener("click", function(){
+    modeButtons[0].classList.remove("selected");
+    modeButtons[1].classList.remove("selected");
+    this.classList.add("selected");
+    if (this.textContent === "Easy"){
+        numberOfSquares=3;
+    }     else {
+        numberOfSquares=6;
+    }
+    reset();
+});
+}
+
+function reset() {
+    colors = generateRandomColors(numberOfSquares);
+    pickedColor = pickColor();
+    choices.textContent = pickedColor;
+    resetButton.textContent = "New Colors";
+    message.textContent = "";
+    for (var i = 0; i < squares.length; i++) {
+        if (colors[i]) {    
+        squares[i].style.display = "block";    
+        squares[i].style.backgroundColor = colors[i];
+    } else {
+        squares[i].style.display = "none";
+    }
+}   h1.style.backgroundColor = "#1d83ac"
+}
+resetButton.addEventListener("click", function() {
+    reset();
+});
+for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+    //add clisk listeners
+    squares[i].addEventListener("click", function () {
+        var clickedColor = this.style.backgroundColor;
+        if (clickedColor === pickedColor) {
+            message.textContent = "Correct!";
+            resetButton.textContent = "Play again?"
+            changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor
+        }else {
+            this.style.backgroundColor = "#eb7200";
+            message.textContent = "Try again!"
+        }
+    });   
+}
+function changeColors(color) {
+    for (var i = 0; i < colors.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+}
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+function generateRandomColors(num) {
+    var arr = []
+    for (var i = 0; i < num; i++) {
+        arr.push(randomColor())
+    } return arr;
+}
+function randomColor() {
+    var r = Math.floor(Math.random()*256);
+    var g = Math.floor(Math.random()*256);
+    var b = Math.floor(Math.random()*256);
+    return "rgb(" + r + ", " + g+", "+b+")";
+}
---
+++ vega/js/world.js
@@ -0,0 +1,37 @@
+const navigation = document.getElementsByClassName("navigation-checkbox");
+const list = document.getElementsByClassName("navigation-list");
+
+$(document).ready(function(){
+    $('.btn-white').hover(function(){
+        $('.btn-white-play').attr('src','/img/play2.png');
+   },function(){
+	$('.btn-white-play').attr('src','/img/play3.png');
+   });
+
+  $ (".navigation-list").on("click", function(event){
+      $(".navigation-nav").hide();
+     $("#navi-toogle").removeClass(); 
+      $("navigation-nav").show();
+   });
+
+});
+
+
+/*var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  slides[slideIndex-1].style.display = "block"; 
+}*/
