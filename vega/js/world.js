e5f27c3650ee9b9a198e0e1530474ed3b6a739f9
Kristian Arbutina <arbutinachristian@gmail.com>
2019-07-06 12:30:27 +0200

    Update world.js

--- vega/js/world.js
+++ vega/js/world.js
@@ -1,4 +1,4 @@
-const navigation = document.getElementsByClassName("navigation-checkbox");
+const navigation = document.getElementsByClassName("navigation-nav");
 const list = document.getElementsByClassName("navigation-list");
 
 $(document).ready(function(){
@@ -11,27 +11,33 @@ $(document).ready(function(){
   /*$ (".navigation-list").on("click", function(event){
       $(".navigation-nav").hide();
      $("#navi-toogle").removeClass(); 
-      $("navigation-nav").show();
+     $("navigation-nav").show();
    });*/
 
 });
 
 
-var slideIndex = 1;
-showSlides(slideIndex);
+var slides = ["fade-1", "fade-2", "fade-3"];
+var showSlides = ["fade-1", "fade-2"];
+
+function slideshow(showSlides) {
+  for(var i = showSlides; i < slides.length; i++ ) {
+    document.getElementsByClassName("photo").style.display = "inline-block";
+  }
+}
 
 // Next/previous controls
-function plusSlides(n) {
+/*function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("photo");
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none"; 
   }
   slides[slideIndex-1].style.display = "block"; 
-}
+}*/
